f9ee9de28b710b5dc2213b26c3161df2ddd95ee7
Aliakbar Salehbigdeli <al.ak.saleh@gmail.com>
2020-11-29 08:23:24 +0330

    basic fstring usage

--- main.py
+++ main.py
@@ -4,4 +4,5 @@ x = 2
 print("I'm going to place a %d instead of %%d in formatting string" % x)
 print("We can do the same thing with format function of a string {0}".format(x))
 print("Or you can use name instead of position like {name}".format(name=x))
-print(f"what do you think about this new style from py3.6: {x}")
+print(f"what do you think about this new style from py3.6: {x**2:02d}")
+raise ValueError(f"Expected {x!r} to be a float not a {type(x).__name__}")
