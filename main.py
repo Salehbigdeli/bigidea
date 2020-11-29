da3f161241b7ba1fd420b3b3b0437f2a4635c393
Aliakbar Salehbigdeli <al.ak.saleh@gmail.com>
2020-11-29 08:18:05 +0330

    Intro to fstrings

---
+++ main.py
@@ -0,0 +1,7 @@
+#  %-formatting strings .format or f''
+
+x = 2
+print("I'm going to place a %d instead of %%d in formatting string" % x)
+print("We can do the same thing with format function of a string {0}".format(x))
+print("Or you can use name instead of position like {name}".format(name=x))
+print(f"what do you think about this new style from py3.6: {x}")
